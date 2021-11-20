584d59a0bd3f9584d139fe4a4d0377f12558553b
ryanpears <pryan78@yahoo.com>
2021-11-19 17:14:14 -0700

    Added  song  model

--- src/spotifyrecommender/client/models.py
+++ src/spotifyrecommender/client/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+# Honestly not sure if this is the correct use for this file
+class SongRec:
+  # add any other feilds we find usefule  here
+  def __init__(self, title, artist, url):
+    self.title = title
+    self.artist = artist
+    self.url  = url
+
--- src/spotifyrecommender/client/views.py
+++ src/spotifyrecommender/client/views.py
@@ -2,7 +2,15 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 
+from .models import SongRec
+
 def index(request):
+  recommended_songs = []
+  for i in range(20):
+    recommended_songs.append(SongRec("Dark Horse", "Katy Parry", "https://open.spotify.com/track/4jbmgIyjGoXjY01XxatOx6"))
   template  = loader.get_template('client/index.html')
-  context = {}
+  context = {
+    'recommended_songs': recommended_songs
+  }
   return HttpResponse(template.render(context, request))
+
