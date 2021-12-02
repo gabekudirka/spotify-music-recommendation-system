4085eb7325b1dfa486616325fa13baff38d08122
edehl <Auggie2018>
2021-12-02 16:04:17 -0700

    added normalization

--- src/top_songs.py
+++ src/top_songs.py
@@ -2,18 +2,33 @@ from objects.spotify_song_attribute_decoder import TrackAttributeDecoder
 import numpy
 
 class TopSongs:
+    def __init__(self):
+        self.attributes = [ "danceability", "energy", "key", "loudness", "mode", "speechiness", "acousticness", "instrumentalness", "liveness", "valence", "tempo"]
+        
+    def normalizeAttr(self, songs):
+        #print(numpy.zeros(len(self.attributes)))
+        sums = dict(zip(self.attributes,numpy.zeros(len(self.attributes))))
+        #print(sums)
+        for key, vals in songs.items():
+            for attribute in self.attributes:
+                sums[attribute]+=vals[attribute]
+        normSongs = songs
+        for key, vals in songs.items():
+            for attribute in self.attributes:
+                normSongs[key][attribute] = vals[attribute]/sums[attribute]
+        return normSongs
 
-    def mse(song, target):
+    def mse(self, song, target):
         totalDif = 0
-        attributes = [ "danceability", "energy", "key", "loudness", "mode", "speechiness", "acousticness", "instrumentalness", "liveness", "valence", "tempo"]
-        for attribute in attributes:
+        for attribute in self.attributes:
             totalDif += (target[attribute] - song[attribute ])**2
         return totalDif
 
-    def getSimilarSongs(songs, target, n):
+    def getSimilarSongs(self, songs, target, n):
+        self.normalizeAttr(songs)
         topSongs = {}
         for key, val in songs.items():
-            score = TopSongs.mse(val,target)
+            score = self.mse(val,target)
             topSongs[key]=score
         sortedSongs = {k: v for k, v in sorted(topSongs.items(), key=lambda item: item[1])}
         #print(sortedSongs)
@@ -26,4 +41,5 @@ if __name__ == '__main__':
     d = TrackAttributeDecoder()
     d.decode_attribute_files()
     #print(d.tracks.keys())
-    print(TopSongs.getSimilarSongs(d.tracks,d.tracks["spotify:track:3G8eHnIYQX6Gbjc5vulISf"],5))
+    t =TopSongs()
+    print(t.getSimilarSongs(d.tracks,d.tracks["spotify:track:3G8eHnIYQX6Gbjc5vulISf"],5))
