82ae6a49775fe2300cf4631fd61e532205cac10e
ryanpears <pryan78@yahoo.com>
2021-12-03 22:07:46 -0700

    parsing into dictionary of dictionaries

--- src/spotifyrecommender/client/views.py
+++ src/spotifyrecommender/client/views.py
@@ -10,6 +10,8 @@ from .models import SongRec
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from settings import SPOTIFY_CLIENT_SECRET, SPOTIFY_CLIENT_ID
+from predict import PredictSongs
+
 
 
 def index(request):
@@ -20,10 +22,9 @@ def index(request):
     if form.is_valid():
       data = form.cleaned_data
       playlist_uri = data['playlist_uri']
-      print("\n\n\n\n look here", playlist_uri)
-    # get_playlist_tracks('https://open.spotify.com/playlist/37i9dQZF1EUMDoJuT8yJsl?si=4f31c7c179524b6a')
-    #TODO recommend songs here
-    
+      playlist_dictionary = get_playlist_tracks(playlist_uri)
+      
+    #TODO send the recomemdations back
     # or not this is pretty solid already
     for i in range(500):
       recommended_songs.append(SongRec("Dark Horse", "Katy Parry", "https://open.spotify.com/track/4jbmgIyjGoXjY01XxatOx6"))
@@ -35,10 +36,46 @@ def index(request):
   return HttpResponse(template.render(context, request))
 
 def get_playlist_tracks(playlist_url):
+    # setup connection
     client_credentials_manager = SpotifyClientCredentials(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET)
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-    path= parse.urlparse(playlist_url).path
-    path, playlist_url = path.split('playlist/',1)
-    print("playlist id is: ", playlist_url)
-    # playlist_items = sp.playlist_items()
+
+    #get playlist id 
+    path = parse.urlparse(playlist_url).path
+    path, playlist_id = path.split('playlist/',1)
+    # get playlist
+    playlist = sp.playlist_items(playlist_id=playlist_id)
+    
+    #parse into dictionary
+    tracks = {}
+    for item in playlist['items']:
+      # get features
+      features = sp.audio_features([item['track']['id']])
+      attributes = {}
+      attributes['danceability'] = features[0]['danceability']
+      attributes['energy']= features[0]['energy']
+      attributes['key']= features[0]['key']
+      attributes['loudness']= features[0]['loudness']
+      attributes['mode']= features[0]['mode']
+      attributes['speechiness']= features[0]['speechiness']
+      attributes['acousticness']= features[0]['acousticness']
+      attributes['instrumentalness']= features[0]['instrumentalness']
+      attributes['liveness']= features[0]['liveness']
+      attributes['valence']= features[0]['valence']
+      attributes['tempo']= features[0]['tempo']
+      attributes['type']= 'audio_features'
+      attributes['id']= item['track']['id']
+      attributes['uri']= item['track']['uri']
+      attributes['track_href']= item['track']['href']
+      attributes['analysis_url']= features[0]['analysis_url']
+      attributes['duration_ms']= item['track']['duration_ms']
+      attributes['time_signature']= features[0]['time_signature']
+      # add to tracks dictionary
+      tracks[f"spotify:track:{item['track']['id']}"] = attributes
+
+    return tracks
+    
+    
+
+    
     
