ae3d58414db9a842c5242e937a93a0a2bc024945
ryanpears <pryan78@yahoo.com>
2021-12-03 19:19:03 -0700

    found error in form and getting playlist id

--- src/spotifyrecommender/client/forms.py
+++ src/spotifyrecommender/client/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class NameForm(forms.Form):
-  your_name = forms.CharField(label='playlist_uri', max_length=100)
+  playlist_uri = forms.CharField(label='playlist_uri', max_length=100)
--- src/spotifyrecommender/client/views.py
+++ src/spotifyrecommender/client/views.py
@@ -1,9 +1,16 @@
+import sys, os
+from urllib import parse
+sys.path.append('../../src')
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 
 from .forms import NameForm
 from .models import SongRec
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from settings import SPOTIFY_CLIENT_SECRET, SPOTIFY_CLIENT_ID
+
 
 def index(request):
   recommended_songs = []
@@ -13,8 +20,10 @@ def index(request):
     if form.is_valid():
       data = form.cleaned_data
       playlist_uri = data['playlist_uri']
-
+      print("\n\n\n\n look here", playlist_uri)
+    # get_playlist_tracks('https://open.spotify.com/playlist/37i9dQZF1EUMDoJuT8yJsl?si=4f31c7c179524b6a')
     #TODO recommend songs here
+    
     # or not this is pretty solid already
     for i in range(500):
       recommended_songs.append(SongRec("Dark Horse", "Katy Parry", "https://open.spotify.com/track/4jbmgIyjGoXjY01XxatOx6"))
@@ -25,3 +34,11 @@ def index(request):
   }
   return HttpResponse(template.render(context, request))
 
+def get_playlist_tracks(playlist_url):
+    client_credentials_manager = SpotifyClientCredentials(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    path= parse.urlparse(playlist_url).path
+    path, playlist_url = path.split('playlist/',1)
+    print("playlist id is: ", playlist_url)
+    # playlist_items = sp.playlist_items()
+    
